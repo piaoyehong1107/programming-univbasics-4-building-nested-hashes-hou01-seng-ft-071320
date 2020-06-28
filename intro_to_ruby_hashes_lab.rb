834de356ec3b37e70a4405d3e06ea139b9d48416
Yehong Piao <piaoyehong1107@gmail.com>
2020-06-28 05:14:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,7 +12,7 @@ end
 def monopoly_with_third_tier
   base_hash ={:railroads =>
   {:pieces =>"4",:rent_in_dollars=>{:one_piece_owne =>25,
-    :two_pieces_owned => 50, 
+    :two_pieces_owned => 50, :three_pieces_owned => 100, 
   },:names=>{} 
     }
   }
