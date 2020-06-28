53fd1eba4ee5bb4743d1a96a4753b8fe734040b9
Yehong Piao <piaoyehong1107@gmail.com>
2020-06-28 05:12:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,10 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-  base_hash ={:railroads =>{:pieces =>"4",:rent_in_dollars=>{}, }}
+  base_hash ={:railroads =>
+  {:pieces =>"4",:rent_in_dollars=>{},:names=>{} 
+    }
+  }
 
 end
 
